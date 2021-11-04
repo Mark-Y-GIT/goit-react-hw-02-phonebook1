2ed2f95e840c00e2546fd32b22bd339ef1f4016d
Mark <yakushyn.mark@gmail.com>
2021-11-04 19:45:34 +0200

    q

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React, { Component } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 // import ContactForm from './components/ContactForm';
 // import ContactList from './components/ContactList';
 
@@ -7,14 +8,27 @@ class App extends Component {
   state = {
     contacts: [],
     name: '',
-    number: '',
   };
 
   handleInputChange = event => {
+    // console.log(event.currentTarget.name);
+    // console.log(event.currentTarget.value);
     const { name, value } = event.currentTarget;
+    this.setState({ name: value });
+    // this.setState({ [name]: [value] });
 
-    this.setState({ [name]: [value] });
+    // console.log(event.currentTarget.value);
   };
+
+  handleOnSubmit = event => {
+    event.preventDefault();
+    this.setState(prevState => {
+      this.cleanInput();
+      return { contacts: [...prevState.contacts, { name: prevState.name }] };
+    });
+  };
+
+  cleanInput = () => this.setState({ name: '' });
 
   render() {
     // const { contacts, name } = this.state;
@@ -22,25 +36,23 @@ class App extends Component {
     return (
       <div>
         <h1>Phonebook</h1>
-        <form>
-          <label>
-            Name
-            <input
-              name="name"
-              value={this.state.name}
-              onChange={this.handleInputChange}
-            ></input>
-          </label>
-
-          <label>
-            Number
-            <input
-              name="number"
-              value={this.state.number}
-              onChange={this.handleInputChange}
-            ></input>
-          </label>
+        <form onSubmit={this.handleOnSubmit}>
+          <input
+            type="text"
+            name="name"
+            value={this.state.name}
+            onChange={this.handleInputChange}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+            required
+          />
+          <button type="submit">Add contact</button>
         </form>
+        <ul>
+          {this.state.contacts.map(contact => (
+            <li id={uuidv4()}>{contact.name}</li>
+          ))}
+        </ul>
       </div>
     );
   }
@@ -52,3 +64,26 @@ export default App;
 // value={this.state.name}
 // onChange={this.handleInputChange}
 // />
+
+// <div>
+//         <h1>Phonebook</h1>
+//         <form>
+//           <label>
+//             Name
+//             <input
+//               name="name"
+//               value={this.state.name}
+//               onChange={this.handleInputChange}
+//             ></input>
+//           </label>
+
+//           <label>
+//             Number
+//             <input
+//               name="number"
+//               value={this.state.number}
+//               onChange={this.handleInputChange}
+//             ></input>
+//           </label>
+//         </form>
+//       </div>
