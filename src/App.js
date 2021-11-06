b7713453935152334085f773f461f28698a90048
Mark <yakushyn.mark@gmail.com>
2021-11-06 19:27:38 +0200

    n3 done

--- src/App.js
+++ src/App.js
@@ -1,56 +1,72 @@
 import './App.css';
 import React, { Component } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import testData from './data.json';
+import { number } from 'prop-types';
 
 class App extends Component {
   state = {
-    contacts: [],
+    contacts: [...testData],
+    filter: '',
     name: '',
     number: '',
   };
-  handleInputChange = event => {
-    const { name, value } = event.currentTarget;
 
+  handleInputChange = e => {
+    const { name, value } = e.target;
     this.setState({ [name]: value });
   };
 
-  handleOnSubmit = event => {
-    event.preventDefault();
+  handleOnSubmit = e => {
+    e.preventDefault();
     this.setState(prevState => {
+      const { name, number } = prevState;
       return {
         contacts: [
           ...prevState.contacts,
-          { name: prevState.name, id: uuidv4(), number: prevState.number },
+          { name: name, id: uuidv4(), number: number },
         ],
       };
     });
+
     this.inputReset();
+  };
+
+  handleFilterChange = e => {
+    const { value } = e.currentTarget;
+    this.setState({ filter: value });
   };
 
   inputReset = () => this.setState({ name: '', number: '' });
 
   render() {
-    // const { contacts, name } = this.state;
-    // console.log(this.state.contacts);
+    const { contacts, name, filter, number } = this.state;
+    const normalizedFiltr = filter.toLowerCase();
+    const filteredList = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFiltr),
+    );
+
+    console.log(filteredList);
 
     return (
       <div>
         <h1>Phonebook</h1>
         <form onSubmit={this.handleOnSubmit}>
+          <h2>Name</h2>
           <input
             type="text"
             name="name"
-            value={this.state.name}
+            value={name}
             onChange={this.handleInputChange}
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
             required
           />
-          <br />
+          <h2>Number</h2>
           <input
             type="tel"
             name="number"
-            value={this.state.number}
+            value={number}
             onChange={this.handleInputChange}
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
@@ -60,8 +76,15 @@ class App extends Component {
           <br />
           <button type="submit">Add contact</button>
         </form>
+        <h2>Filter</h2>
+        <input
+          type="text"
+          name="filter"
+          value={filter}
+          onChange={this.handleFilterChange}
+        />
         <ul>
-          {this.state.contacts.map(contact => (
+          {filteredList.map(contact => (
             <li id={contact.id} key={contact.id}>
               <span>{contact.name}</span> {contact.number}
             </li>
@@ -75,3 +98,19 @@ class App extends Component {
 export default App;
 
 //
+
+// contact.name.toLocaleLowerCase().includes(filter.toLocaleLowerCase());
+
+//  <ul>
+//    {filter.length
+//      ? filteredList.map(contact => (
+//          <li id={contact.id} key={contact.id}>
+//            <span>{contact.name}</span> {contact.number}
+//          </li>
+//        ))
+//      : contacts.map(contact => (
+//          <li id={contact.id} key={contact.id}>
+//            <span>{contact.name}</span> {contact.number}
+//          </li>
+//        ))}
+//  </ul>;
